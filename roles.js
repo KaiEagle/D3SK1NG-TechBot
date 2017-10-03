170806a25777ffcd4afe79193bba3da2d1886ac3
KaiEagle <boster8701@gmail.com>
2017-10-03 18:22:25 +0530

    Update roles.js

--- roles.js
+++ roles.js
@@ -7,9 +7,9 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
         .setColor(Math.floor(Math.random() * 16777215))
         .setAuthor(`${message.author.username}`, message.author.avatarURL)
-        .addField('Need your Discord role?', 'To obtain your Discord role you must have proof of purchase (ex.Paypal Transaction or Admin Conversations), without proof of purchase, Admins/Supervisors cannot give you your role.')
-        .addField('Need your Forums role?', 'To obtain your Forum role you must have proof of purchase (ex.Paypal Transaction & Admin Conversations), if you wish to have your forum role PM Supervisor with your Forum Name and Role')
-        .setFooter('Please tell a staff member if you have more questions.')
+        .addField('Need your Discord role?', 'To obtain your Discord role you must have proof of purchase (ex.Paypal Transaction or Admin Conversations), without proof of purchase, Admins cannot give you your role.')
+        .addField('Need your Forums role?', 'To obtain your Forum role you must have proof of purchase (ex.Paypal Transaction & Admin Conversations), if you wish to have your forum role PM Admin with your Forum Name and Role')
+        .setFooter('Please ask a staff member if you have more questions.')
     message.channel.send({embed})
 
 
