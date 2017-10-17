d96daf415fb4c352d7866c66639d6a936b757125
KaiEagle <boster8701@gmail.com>
2017-10-17 17:18:10 +0530

    Update vsp.js

--- vsp.js
+++ vsp.js
@@ -9,7 +9,7 @@
             .addField('What is the differences between VIP & Premium', 'The differences are, Better Features, Better Support')
             .addField('Is any of them safer than the other?', 'No, They are both equally safe.')
             .addField('VIP Version', 'VIP Version of the Menu has better options as compared to Premium Version which can be used to your advantages, such as, **2bil per 1/2 second**, 600k Stealth, troll options such as Infinite Black Screen, delayed non-host kick, host kick and many more...')
-            .addField('Premium Version', 'Premium Version has all the default features & would work perfectly too but just without massive trollings & greifing features.')
+            .addField('Premium Version', 'Premium Version has all the default features & would work perfectly too but just without massive trollings & griefing features.')
             .addField('Have a new & awesome feature suggestion(s) in mind?', 'Create a thread and post all of your suggestions [here](https://d3sk1ng.com/index.php?forums/update-suggestions.48/)')
             .setFooter('Alternatively, You can post your feature suggestions in our discord channel in #suggestions');
         message.delete();
