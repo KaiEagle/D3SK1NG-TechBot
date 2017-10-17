c3401bb87751fc377a0a7dfa08d7b3d50ee2d9eb
KaiEagle <boster8701@gmail.com>
2017-10-17 17:02:45 +0530

    Update roles.js

--- roles.js
+++ roles.js
@@ -8,7 +8,6 @@ exports.run = (client, message, args) => {
         .setColor(Math.floor(Math.random() * 16777215))
         .setAuthor(`${message.author.username}`, message.author.avatarURL)
         .addField('Need your Discord role?', 'To obtain your Discord role you must have proof of purchase (ex.Paypal Transaction or Admin Conversations), without proof of purchase, Admins cannot give you your role.')
-        .addField('Need your Forums role?', 'To obtain your Forum role you must have proof of purchase (ex.Paypal Transaction & Admin Conversations), if you wish to have your forum role PM Admin with your Forum Name and Role')
         .setFooter('Please ask a staff member if you have more questions.')
     message.channel.send({embed})
 
