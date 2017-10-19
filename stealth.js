373db5cd87fbd18def2d5d4501fbd4782378db51
KaiEagle <boster8701@gmail.com>
2017-10-20 01:40:42 +0530

    Update stealth.js

--- stealth.js
+++ stealth.js
@@ -5,9 +5,10 @@ exports.run = (client, message, args) => {
     const embed = new Discord.RichEmbed()
         .setColor(Math.floor(Math.random() * 16777215))
         .setAuthor(`${message.author.username}`, message.author.avatarURL)
-        .addField('How to use stealth mode?', 'The stealth mode is not like the old stealth, to make it work you have to actually trigger a transaction. How? Sell a car, pick up some money from a ped etc.')
-        .addField('Text Guode:', '1. Go to Recovery>Stealth Mode>Add Money to: (Bank/cash).\n2. Go to Recovery>Stealth Mode>Amount(Choose how much you want per sec)\n3. Go to Recovery>Stealth Mode>Status>"Tick it"\n4. Go to Recovery>Stealth Mode>Self Ped Drop>"tick it" \n\nThat\'s it. Now you will get the "amount" you inputted!')
-        .addField('GIF Guide', '[Click Here](https://imgur.com/AhtBnNh)')
+        .addField('How to use stealth "correctly and not get kicked?', 'The stealth mode is not like the old stealth, to make it work you have to actually trigger a transaction. How? Sell a car, pick up some money from a ped etc.')
+        .addField('Why you get kicked?You trigger transcation too fast to a point where your internet cant process it.'
+        .addField('Text Guide:', '1. Go to Recovery > Stealth > Choose the amount you want. > Enable stealth mode.\n2. Go down to self ped drops.(Recovery > stealth > self ped drop)\n3. SET THE DELAY to 550(IF 500 kicks you offline try 1500) > Enable status'
+        .addfield('DONT PICK UP THE MONEY THAT SPAWNS AROUND YOU', 'OR', '1. Go to Recovery>Stealth > Choose the amount you want.>Enable stealth mode.\n2. Go down to self money bag drops.(Recovery > stealth>self money bag drop).\n3 Set the delay to (1800-2800) > enable status.'
         .addField('Note:', 'VIP users get 10mil stealth while Premium users get 5mil.')
         .setThumbnail('')
         .setFooter('');
